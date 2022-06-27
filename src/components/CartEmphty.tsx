8c682de49d8e137863fc4e9563409e750915c617
ike-s1 <vitalik.fedorchak1111#gmail.com>
2022-06-27 12:26:45 +0300

    ready

--- src/components/CartEmphty.tsx
+++ src/components/CartEmphty.tsx
@@ -1,21 +1,22 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
-import cartEmptyimg  from '../assets/img/empty-cart.png'
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import cartEmptyimg from "../assets/img/empty-cart.png";
 
-const CartEmphty:FC = () => {
-    return (
-        <div className="cart cart--empty">
-            <h2>Корзина пустая</h2> <span className='emodji'>😕</span>
-            <p>
-            Вероятней всего, вы не заказывали ещё пиццу.<br />
-            Для того, чтобы заказать пиццу, перейди на главную страницу.
-            </p>
-            <img  src={cartEmptyimg} alt="EmptyCart" />
-            <Link to="/" className="button button--black">
-            <span>Вернуться назад</span>
-            </Link>
-      </div>
-    );
+const CartEmphty: FC = () => {
+  return (
+    <div className="cart cart--empty">
+      <h2>Cart is empty</h2> <span className="emodji">😕</span>
+      <p>
+        You probably haven't ordered pizza yet.
+        <br />
+        To order pizza, go to the main page.
+      </p>
+      <img src={cartEmptyimg} alt="EmptyCart" />
+      <Link to="/" className="button button--black">
+        <span>Come back</span>
+      </Link>
+    </div>
+  );
 };
 
 export default CartEmphty;
